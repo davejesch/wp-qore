99fbc5ebf63e55ca31c1ee2e82f7b1b427af6f94
Jason <icryptic@users.noreply.github.com>
2013-11-29 19:38:52 -0700

    Update dashboard_view.php

--- wp-content/plugins/wp-qore/functions/dashboard_view.php
+++ wp-content/plugins/wp-qore/functions/dashboard_view.php
@@ -1,4 +1,6 @@
-<?php ?>
+<?php //wp-admin dashboard : begin 
+
+?>
 
 <div class="wrap about-wrap" id="pbody">
 <div style="float:right;font-size:12px;font-weight:normal;color:#333">WP Qore: <b><?php wpqoreplugv(); ?></b></div>
@@ -10,38 +12,43 @@
 
 <div style="margin-top:0px">
 
-<div class="dashboard_icons"><div  class="dash_text">Posts</div><a href="edit.php" title="Posts"><img src="../wp-content/plugins/wp-qore/images/dashboard/edit.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Add Posts</div><a href="post-new.php" title="Add New Post"><img src="../wp-content/plugins/wp-qore/images/dashboard/add.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Categories</div><a href="edit-tags.php?taxonomy=category" title="Categories"><img src="../wp-content/plugins/wp-qore/images/dashboard/category.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Tags</div><a href="edit-tags.php?taxonomy=post_tag" title="Tags"><img src="../wp-content/plugins/wp-qore/images/dashboard/tags.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Media Library</div><a href="upload.php" title="Media Library"><img src="../wp-content/plugins/wp-qore/images/dashboard/media.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Add Media</div><a href="media-new.php" title="Add New Media"><img src="../wp-content/plugins/wp-qore/images/dashboard/media_add.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Comments</div><a href="edit-comments.php" title="Comments"><img src="../wp-content/plugins/wp-qore/images/dashboard/comments.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Add Page</div><a href="edit.php?post_type=page" title="Add New Page"><img src="../wp-content/plugins/wp-qore/images/dashboard/add_page.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Themes</div><a href="themes.php" title="Themes"><img src="../wp-content/plugins/wp-qore/images/dashboard/themes.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Widgets</div><a href="widgets.php" title="Widgets"><img src="../wp-content/plugins/wp-qore/images/dashboard/widgets.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Menus</div><a href="nav-menus.php" title="Menus"><img src="../wp-content/plugins/wp-qore/images/dashboard/menus.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Theme Editor</div><a href="theme-editor.php" title="Theme Editor"><img src="../wp-content/plugins/wp-qore/images/dashboard/theme_editor.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Plugins</div><a href="plugins.php" title="Plugins"><img src="../wp-content/plugins/wp-qore/images/dashboard/plugins.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Add Plugin</div><a href="plugin-install.php" title="Add New Plugin"><img src="../wp-content/plugins/wp-qore/images/dashboard/add-plugin.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Plugins Editor</div><a href="plugin-editor.php" title="Plugin Editor"><img src="../wp-content/plugins/wp-qore/images/dashboard/edit-plugin.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Users</div><a href="users.php" title="Users"><img src="../wp-content/plugins/wp-qore/images/dashboard/users.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Add User</div><a href="user-new.php" title="Add New User"><img src="../wp-content/plugins/wp-qore/images/dashboard/add-user.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">My Profile</div><a href="profile.php" title="My Profile"><img src="../wp-content/plugins/wp-qore/images/dashboard/profile.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Import</div><a href="import.php" title="Import Content"><img src="../wp-content/plugins/wp-qore/images/dashboard/import.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Export</div><a href="export.php" title="Export Content"><img src="../wp-content/plugins/wp-qore/images/dashboard/export.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">General</div><a href="options-general.php" title="General Settings"><img src="../wp-content/plugins/wp-qore/images/dashboard/general.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Writing</div><a href="options-writing.php" title="Writing Settings"><img src="../wp-content/plugins/wp-qore/images/dashboard/writing-settings.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Reading</div><a href="options-reading.php" title="Reading Settings"><img src="../wp-content/plugins/wp-qore/images/dashboard/reading-settings.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Discussion</div><a href="options-discussion.php" title="Discussion Settings"><img src="../wp-content/plugins/wp-qore/images/dashboard/discussion-settings.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Media</div><a href="options-media.php" title="Media Settings"><img src="../wp-content/plugins/wp-qore/images/dashboard/media-settings.png"/></a></div>
-<div class="dashboard_icons"><div  class="dash_text">Permalink</div><a href="options-permalink.php" title="Permalink Settings"><img src="../wp-content/plugins/wp-qore/images/dashboard/permalink.png"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Posts</div><a href="edit.php" title="Posts"><img src="<?php echo plugins_url( '../images/dashboard/edit.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Add Posts</div><a href="post-new.php" title="Add New Post"><img src="<?php echo plugins_url( '../images/dashboard/add.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Categories</div><a href="edit-tags.php?taxonomy=category" title="Categories"><img src="<?php echo plugins_url( '../images/dashboard/category.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Tags</div><a href="edit-tags.php?taxonomy=post_tag" title="Tags"><img src="<?php echo plugins_url( '../images/dashboard/tags.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Media Library</div><a href="upload.php" title="Media Library"><img src="<?php echo plugins_url( '../images/dashboard/media.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Add Media</div><a href="media-new.php" title="Add New Media"><img src="<?php echo plugins_url( '../images/dashboard/media_add.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Comments</div><a href="edit-comments.php" title="Comments"><img src="<?php echo plugins_url( '../images/dashboard/comments.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Add Page</div><a href="edit.php?post_type=page" title="Add New Page"><img src="<?php echo plugins_url( '../images/dashboard/add_page.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Themes</div><a href="themes.php" title="Themes"><img src="<?php echo plugins_url( '../images/dashboard/themes.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Widgets</div><a href="widgets.php" title="Widgets"><img src="<?php echo plugins_url( '../images/dashboard/widgets.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Menus</div><a href="nav-menus.php" title="Menus"><img src="<?php echo plugins_url( '../images/dashboard/menus.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Theme Editor</div><a href="theme-editor.php" title="Theme Editor"><img src="<?php echo plugins_url( '../images/dashboard/theme_editor.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Plugins</div><a href="plugins.php" title="Plugins"><img src="<?php echo plugins_url( '../images/dashboard/plugins.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Add Plugin</div><a href="plugin-install.php" title="Add New Plugin"><img src="<?php echo plugins_url( '../images/dashboard/add-plugin.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Plugins Editor</div><a href="plugin-editor.php" title="Plugin Editor"><img src="<?php echo plugins_url( '../images/dashboard/edit-plugin.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Users</div><a href="users.php" title="Users"><img src="<?php echo plugins_url( '../images/dashboard/users.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Add User</div><a href="user-new.php" title="Add New User"><img src="<?php echo plugins_url( '../images/dashboard/add-user.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">My Profile</div><a href="profile.php" title="My Profile"><img src="<?php echo plugins_url( '../images/dashboard/profile.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Import</div><a href="import.php" title="Import Content"><img src="<?php echo plugins_url( '../images/dashboard/import.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Export</div><a href="export.php" title="Export Content"><img src="<?php echo plugins_url( '../images/dashboard/export.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">General</div><a href="options-general.php" title="General Settings"><img src="<?php echo plugins_url( '../images/dashboard/general.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Writing</div><a href="options-writing.php" title="Writing Settings"><img src="<?php echo plugins_url( '../images/dashboard/writing-settings.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Reading</div><a href="options-reading.php" title="Reading Settings"><img src="<?php echo plugins_url( '../images/dashboard/reading-settings.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Discussion</div><a href="options-discussion.php" title="Discussion Settings"><img src="<?php echo plugins_url( '../images/dashboard/discussion-settings.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Media</div><a href="options-media.php" title="Media Settings"><img src="<?php echo plugins_url( '../images/dashboard/media-settings.png' , __FILE__ ); ?>"/></a></div>
+<div class="dashboard_icons"><div  class="dash_text">Permalink</div><a href="options-permalink.php" title="Permalink Settings"><img src="<?php echo plugins_url( '../images/dashboard/permalink.png' , __FILE__ ); ?>"/></a></div>
 
 </div>
 </div>
 
 <style type="text/css" media="all" >
-@import url("../wp-content/plugins/wp-qore/css/dashboard.css");
+@import url("<?php echo plugins_url( '../css/dashboard.css' , __FILE__ ); ?>");
 </style>
 
-<?php include( ABSPATH . 'wp-admin/admin-footer.php' ); ?>
+<?php 
+
+include( ABSPATH . 'wp-admin/admin-footer.php' ); 
+
+//wp-admin dashboard : end 
+ ?>
