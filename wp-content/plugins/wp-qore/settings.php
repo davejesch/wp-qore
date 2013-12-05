2ed537c324a9a677eb00fcd8aeb77ee5a575269e
Jason <icryptic@users.noreply.github.com>
2013-12-05 07:04:51 -0700

    Update settings.php

--- wp-content/plugins/wp-qore/settings.php
+++ wp-content/plugins/wp-qore/settings.php
@@ -221,7 +221,8 @@ When checked, this will replace the standard wp-admin dashboard with a new one t
 <label><i></i></label>
 </div>
 <i>Check to enable</i><br>
-When checked, this will enable the WP Qore tabs on the new Dashboard. The 'Replace Dashboard' option must be 'On' in order to use this feature.
+When checked, this will enable the WP Qore tabs on the new Dashboard.<br>
+<b>Attention:</b> The 'Replace Dashboard' option must be 'On' in order to use this feature.
 </label>
 </td>
 </tr>
