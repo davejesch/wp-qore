6392e981caead8990bc32c19c6d5a02caf9fb84a
Jason <icryptic@users.noreply.github.com>
2013-12-08 00:41:17 -0700

    Delete gzip.php

--- wp-content/plugins/wp-qore/functions/gzip.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(extension_loaded("zlib") && (ini_get("output_handler") != "ob_gzhandler"))
-   add_action('wp', create_function('', '@ob_end_clean();@ini_set("zlib.output_compression", 1);'));
-
-?>
