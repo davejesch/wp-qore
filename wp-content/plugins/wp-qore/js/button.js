e67321a223ded299ab7c3e80be949842bc7adc04
Jason Jersey <icryptic@users.noreply.github.com>
2014-01-03 17:37:57 -0700

    Update button.js

--- wp-content/plugins/wp-qore/js/button.js
+++ wp-content/plugins/wp-qore/js/button.js
@@ -1,17 +1,16 @@
-(function() {
-    tinymce.create('tinymce.plugins.Wpfc', {
-        init : function(ed, url) {
-            url = url.replace("../js","../images");
-            ed.addButton('wpfc', {
-                title : 'Block caching for this page',
-                cmd : 'wpfc',
-                image : url + "/icon.png"
-            });
-
-            ed.addCommand('wpfc', function() {
-                ed.execCommand('mceInsertContent', 0, "[wpfcNOT]");
-            });
-        }
-    });
-    tinymce.PluginManager.add( 'wpfc', tinymce.plugins.Wpfc );
+(function() {
+    tinymce.create('tinymce.plugins.wpqca', {
+        init : function(ed, url) {
+            url = url.replace("../js","../images");
+            ed.addButton('wpqca', {
+                title : 'Block caching for this page',
+                cmd : 'wpqca',
+                image : url + "/icon.png"
+            });
+            ed.addCommand('wpqca', function() {
+                ed.execCommand('mceInsertContent', 0, "[NoCache]");
+            });
+        }
+    });
+    tinymce.PluginManager.add( 'wpqca', tinymce.plugins.wpqca );
 })();
