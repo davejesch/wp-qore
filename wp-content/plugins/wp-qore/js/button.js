3202ce802a73b62fed076c3a06861adf3a3337ce
Jason <jasonjersey@degersey.com>
2014-01-08 10:08:27 -0700

    commit new changes

--- wp-content/plugins/wp-qore/js/button.js
+++ wp-content/plugins/wp-qore/js/button.js
@@ -1,16 +1,16 @@
-(function() {
-    tinymce.create('tinymce.plugins.wpqca', {
-        init : function(ed, url) {
-            url = url.replace("../js","../images");
-            ed.addButton('wpqca', {
-                title : 'Block caching for this page',
-                cmd : 'wpqca',
-                image : url + "/icon.png"
-            });
-            ed.addCommand('wpqca', function() {
-                ed.execCommand('mceInsertContent', 0, "[NoCache]");
-            });
-        }
-    });
-    tinymce.PluginManager.add( 'wpqca', tinymce.plugins.wpqca );
-})();
+(function() {
+    tinymce.create('tinymce.plugins.wpqca', {
+        init : function(ed, url) {
+            url = url.replace("../js","../images");
+            ed.addButton('wpqca', {
+                title : 'Block caching for this page',
+                cmd : 'wpqca',
+                image : url + "/icon.png"
+            });
+            ed.addCommand('wpqca', function() {
+                ed.execCommand('mceInsertContent', 0, "[NoCache]");
+            });
+        }
+    });
+    tinymce.PluginManager.add( 'wpqca', tinymce.plugins.wpqca );
+})();
