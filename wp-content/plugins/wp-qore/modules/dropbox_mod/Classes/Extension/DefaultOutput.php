ae69578db3464513fef8ead2bc100195014bddf2
Jason Jersey <icryptic@users.noreply.github.com>
2014-01-14 02:19:40 -0700

    Update DefaultOutput.php

--- wp-content/plugins/wp-qore/modules/dropbox_mod/Classes/Extension/DefaultOutput.php
+++ wp-content/plugins/wp-qore/modules/dropbox_mod/Classes/Extension/DefaultOutput.php
@@ -2,8 +2,7 @@
 /**
  * A class with functions the perform a backup of WordPress
  *
- * @copyright Copyright (C) 2011-2013 Michael De Wild. All rights reserved.
- * @author Michael De Wildt (http://www.mikeyd.com.au/)
+ * @author Michael De Wildt 
  * @license This program is free software; you can redistribute it and/or modify
  *          it under the terms of the GNU General Public License as published by
  *          the Free Software Foundation; either version 2 of the License, or
