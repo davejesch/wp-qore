315c9a4306035bd91abc7e1d4f241e52058614e3
Jason Jersey <icryptic@users.noreply.github.com>
2014-01-17 02:52:07 -0700

    Update index.php

--- wp-content/plugins/wp-qore/index.php
+++ wp-content/plugins/wp-qore/index.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Qore
 Plugin URI: http://wpqore.com/
 Description: WP Qore is a WordPress plugin that provides additional security, performance functionality, and developer tools that can be toggled on or off at anytime.
-Version: 1.7.5
+Version: 1.7.6
 Author: Jason Jersey
 Author URI: http://twitter.com/degersey
 License: GNU GPL 3.0
@@ -27,7 +27,7 @@ Domain Path: lang
 
 // wp-qore version
 function wpqoreplugv() {
-    echo '1.7.5';
+    echo '1.7.6';
 }
 
 function wpqore_load_textdomain() {
